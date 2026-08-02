fa7729403fe6fac06a323a5e14531f2a94f99a26
77777kaz77777 <153332224+77777kaz77777@users.noreply.github.com>
2026-08-01 22:13:57 -0400

    Update and rename user2.sh to create_user_interactive.sh

--- provisioning/create_user_interactive.sh
+++ provisioning/create_user_interactive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- # Helper script for linux user creation
+# Safely creates a user account, configures passwords, and optionally grants administrative privileges.
 # Function to create a new user
 create_user() {
   local USERNAME=$1
